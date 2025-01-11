d6567ad8629ef4e048582b577e2a3959f1de28a2
yozhgoor <yozhgoor@outlook.com>
2025-01-11 04:03:33 +0100

    Add the `list_contacts` handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs;
+use std::io::Write;
 use std::path::PathBuf;
 use std::sync::Arc;
 
@@ -5,17 +7,16 @@ use axum::{
     extract::{Path as AxumPath, State},
     http::StatusCode,
     response::IntoResponse,
-    routing::{get, post},
+    routing::get,
     Json, Router,
 };
 use directories::ProjectDirs;
 use serde::{Deserialize, Serialize};
-use tokio::{fs, io::AsyncWriteExt};
 use tracing::{error, info};
 
 const ADDR: &str = "127.0.0.1:3000";
 
-#[derive(Deserialize, Serialize, Debug)]
+#[derive(Default, Deserialize, Serialize, Debug)]
 struct Contact {
     id: String,
     name: String,
@@ -35,7 +36,7 @@ async fn main() {
     let base_path = ProjectDirs::from("", "", "dav").expect("failed to determine base directories");
     let data_dir = base_path.data_dir().join("contacts");
 
-    if let Err(e) = tokio::fs::create_dir_all(&data_dir).await {
+    if let Err(e) = fs::create_dir_all(&data_dir) {
         error!("failed to create contact directory: {}", e);
         return;
     }
@@ -47,8 +48,8 @@ async fn main() {
 
     let app = Router::new()
         .route("/health", get(health_check))
+        .route("/contacts", get(list_contacts).post(create_contact))
         .route("/contacts/:id", get(contact_by_id))
-        .route("/contacts", post(create_contact))
         .with_state(Arc::new(state));
 
     let listener = match tokio::net::TcpListener::bind(ADDR).await {
@@ -73,23 +74,26 @@ async fn create_contact(
     State(state): State<Arc<AppState>>,
     Json(contact): Json<Contact>,
 ) -> impl IntoResponse {
-    let file_path = format!("{}/{}.vcf", state.data_dir.display(), contact.id);
+    let file_path = state.data_dir.join(contact.id);
 
     let vcard = format!(
         "BEGIN:VCARD\nVERSION:4.0\nFN:{}\nEMAIL:{}\nTEL:{}\nEND:VCARD\n",
         contact.name, contact.email, contact.phone
     );
 
-    match fs::File::create(&file_path).await {
+    match fs::File::create(&file_path) {
         Ok(mut file) => {
-            if let Err(e) = file.write_all(vcard.as_bytes()).await {
+            if let Err(e) = file.write_all(vcard.as_bytes()) {
                 error!("Error writing to file: {}", e);
                 return (StatusCode::INTERNAL_SERVER_ERROR, "failed to save contact");
             }
 
             (StatusCode::CREATED, "Contact created")
         }
-        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "failed to create file"),
+        Err(e) => {
+            error!("failed to create file at {}: {}", file_path.display(), e);
+            (StatusCode::INTERNAL_SERVER_ERROR, "failed to create file")
+        }
     }
 }
 
@@ -97,10 +101,82 @@ async fn contact_by_id(
     AxumPath(id): AxumPath<String>,
     State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
-    let file_path = format!("{}/{}.vcf", state.data_dir.display(), id);
+    let file_path = state.data_dir.join(id);
 
-    match fs::read_to_string(&file_path).await {
-        Ok(content) => (StatusCode::OK, content),
-        Err(_) => (StatusCode::NOT_FOUND, "Contact not found".to_string()),
+    match fs::read_to_string(&file_path) {
+        Ok(content) => {
+            info!("Contact found at {}", file_path.display());
+            (StatusCode::OK, content)
+        }
+        Err(e) => {
+            error!("contact not found at {}: {}", file_path.display(), e);
+            (StatusCode::NOT_FOUND, "Contact not found".to_string())
+        }
+    }
+}
+
+async fn list_contacts(
+    State(state): State<Arc<AppState>>,
+) -> Result<(StatusCode, Json<Vec<Contact>>), (StatusCode, String)> {
+    match fs::read_dir(&*state.data_dir) {
+        Ok(entries) => {
+            let mut contacts = Vec::new();
+            for entry in entries.filter_map(Result::ok) {
+                let path = entry.path();
+
+                let id = match path.file_stem().and_then(|stem| stem.to_str()) {
+                    Some(stem) => stem.to_string(),
+                    None => {
+                        error!("invalid file name: {}", path.display());
+                        return Err((
+                            StatusCode::INTERNAL_SERVER_ERROR,
+                            "invalid file name".to_string(),
+                        ));
+                    }
+                };
+
+                if let Ok(content) = fs::read_to_string(&path) {
+                    if let Some(contact) = parse_vcard(id, content) {
+                        contacts.push(contact);
+                    }
+                }
+            }
+
+            info!("Contacts list created successfully");
+            Ok((StatusCode::OK, Json(contacts)))
+        }
+        Err(e) => {
+            error!("failed to list contacts: {}", e);
+            Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "failed to list contacts".to_string(),
+            ))
+        }
+    }
+}
+
+fn parse_vcard(id: String, vcard: String) -> Option<Contact> {
+    let mut name = None;
+    let mut email = None;
+    let mut phone = None;
+
+    for line in vcard.lines() {
+        if line.starts_with("FN:") {
+            name = Some(line.trim_start_matches("FN:").to_string());
+        } else if line.starts_with("EMAIL:") {
+            email = Some(line.trim_start_matches("EMAIL:").to_string());
+        } else if line.starts_with("TEL:") {
+            phone = Some(line.trim_start_matches("TEL:").to_string());
+        }
+    }
+
+    match (name.as_ref(), email.as_ref(), phone.as_ref()) {
+        (None, None, None) => None,
+        _ => Some(Contact {
+            id,
+            name: name.unwrap_or_default(),
+            email: email.unwrap_or_default(),
+            phone: phone.unwrap_or_default(),
+        }),
     }
 }
